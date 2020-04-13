86bf6990aeb7d2b79479cddda084114c7df07d04
Stuart Myers <stuart.myers@gmail.com>
2020-04-13 15:36:41 +0800

    Adding blog post summaries

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
-import BlogPostSummaries from "../components/blogpostsummaries"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi There!</h1>
     <p>Welcome to Stuart's website.</p>
-    <BlogPostSummaries />
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
