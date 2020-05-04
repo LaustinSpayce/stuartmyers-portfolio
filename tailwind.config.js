f80ea3e79c483e5c058b8bdd95a0a1af5cb70823
Stuart Myers <stuart.myers@gmail.com>
2020-05-04 14:02:38 +0800

    Customised Tailwind, heights now ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      cyan: '#9cdbff',
+    },
+    spacing: {
+      '90': '21rem',
+      '96': '24rem',
+      '128': '32rem',
+    }},
   },
   variants: {},
   plugins: [],
